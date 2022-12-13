b0df764779691fdf4144c1e957705eb2b0109ddb
UdohYoungPJ <praisejahyoung@gmail.com>
2022-12-13 13:49:09 -0800

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
+
 void print_alphabet(void)
 {
-  char ch;
-  ch = 'a';
-
-  while (ch <= 'z')
-    {
-      _putchar(ch);
-      ch++; 
-    }
-  _putchar('\n'); 
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
 }
